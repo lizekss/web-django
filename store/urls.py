63f1801168fa64e20b73c77727384cb0ea46d739
lizekss <lekse20@freeuni.edu.ge>
2024-10-10 18:34:31 +0400

    views

--- store/urls.py
+++ store/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('products/', views.product_list, name='product_list'),
     path('products/<int:id>/', views.product_detail, name='product_detail'),
+    path('categories/', views.category_list, name='category_list'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+from store.models import Product, Category
 
 
 def product_list(request):
-    return HttpResponse("This is the product list.")
+    products = Product.objects.all()
+    data = []
+    for product in products:
+        categories = product.categories.all()
+        categories_list = [{'id': cat.id, 'name': cat.name}
+                           for cat in categories]
+        data.append({
+            'id': product.id,
+            'name': product.name,
+            'price': str(product.price),
+            'categories': categories_list
+        })
+    return JsonResponse(data, safe=False)
 
 
 def product_detail(request):
     return HttpResponse("This is the product detail.")
+
+
+def category_list(request):
+    categories = Category.objects.all()
+    data = []
+    for category in categories:
+        data.append({
+            'id': category.id,
+            'name': category.name,
+            'parent': category.parent.name if category.parent else None
+        })
+    return JsonResponse(data, safe=False)
