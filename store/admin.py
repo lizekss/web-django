5f5f4234f904fbd78ebe43fc8ce157fcb7c8b2c5
lizekss <lekse20@freeuni.edu.ge>
2024-10-10 16:00:05 +0400

    models

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from store.models import Category, Product
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'parent', 'get_full_path')
+    search_fields = ('name',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product)
--- store/models.py
+++ store/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    parent = models.ForeignKey(
+        'self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    def __str__(self):
+        return self.name
+
+    def get_full_path(self):
+        """
+        Returns the full path of the category, including all parent categories.
+        """
+        if self.parent:
+            return f"{self.parent.get_full_path()} > {self.name}"
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    categories = models.ManyToManyField(Category, related_name='products')
+
+    def __str__(self):
+        return self.name
