eb4cd400481d5fe7e3723b2d47bcfde5d9106a67
lizekss <lekse20@freeuni.edu.ge>
2024-10-10 16:10:51 +0400

    add image field

--- store/models.py
+++ store/models.py
@@ -23,6 +23,7 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     categories = models.ManyToManyField(Category, related_name='products')
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
 
     def __str__(self):
         return self.name
