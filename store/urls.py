4fc25594fdef23451e871722de064ea38144cd24
lizekss <lekse20@freeuni.edu.ge>
2024-10-15 18:34:14 +0400

    categories url

--- store/urls.py
+++ store/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('products/', views.product_list, name='product_list'),
     path('products/<int:product_id>/',
          views.product_detail, name='product_detail'),
-    path('categories/', views.category_list, name='category_list'),
-    path('categories/<int:category_id>/products',
+    path('category/', views.category_list, name='category_list'),
+    path('category/<int:category_id>/products',
          views.category_detail, name='category_detail'),
 ]
