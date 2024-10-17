258f1a18e2c67a065aff36133631f6f61d586c72
lizekss <lekse20@freeuni.edu.ge>
2024-10-17 18:43:31 +0400

    admin panel optimizations

--- store/admin.py
+++ store/admin.py
@@ -3,10 +3,26 @@ from django.contrib import admin
 from store.models import Category, Product
 
 
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity',
+                    'total_stock_value', 'get_categories')
+    list_filter = ('categories', 'price')
+    search_fields = ('name', 'description')
+    ordering = ('name',)
+
+    def total_stock_value(self, obj):
+        return obj.price * obj.quantity
+    total_stock_value.short_description = 'Total Stock Value'
+
+    def get_categories(self, obj):
+        return ", ".join([category.name for category in obj.categories.all()])
+    get_categories.short_description = 'Categories'
+
+
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'parent')
+    list_filter = ('parent',)
     search_fields = ('name',)
-
-
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Product)
+    ordering = ('name',)
--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import MyUser
 
-from user.models import MyUser
 
-admin.site.register(MyUser)
+@admin.register(MyUser)
+class MyUserAdmin(UserAdmin):
+    model = MyUser
+    list_display = ('email', 'first_name', 'last_name',
+                    'date_of_birth', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'is_superuser', 'groups')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {
+         'fields': ('first_name', 'last_name', 'date_of_birth')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active',
+         'is_superuser', 'groups', 'user_permissions')}),
+    )
