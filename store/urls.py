19c8c6ca49ef83710af5652ce4fece29bdecca20
lizekss <lekse20@freeuni.edu.ge>
2024-10-31 16:51:01 +0400

    basic login

--- store/urls.py
+++ store/urls.py
@@ -1,12 +1,8 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
 from . import views
 from .views import CategoryListView, HomeView, ProductDetailView, ContactView
 
-# urlpatterns = [
-#     path('products/', views.product_list, name='product_list'),
-#     path('category/<int:category_id>/products',
-#          views.category_detail, name='category_detail'),
-# ]
 
 urlpatterns = [
     path('', HomeView.as_view(), name="index"),
@@ -16,4 +12,5 @@ urlpatterns = [
     path('product/<int:pk>/',
          ProductDetailView.as_view(), name='product_detail'),
     path('contact/', ContactView.as_view(), name='contact'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
 ]
