ee6b235d02090298758cab7ae0e1fe1d9dd4e21b
lizekss <lekse20@freeuni.edu.ge>
2024-11-08 23:05:18 +0400

    i18n url patterns

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,19 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.static import serve
 
-import store
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),
     path('order/', include('order.urls')),
     path('user/', include('user.urls', namespace='user')),
-]
+    prefix_default_language=False,
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
